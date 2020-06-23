fad1594c199d9f94fc8c3725f379d870b487b635
= <=>
2020-06-23 15:54:58 +0300

    Add AWS ECR repos for each container.upload_docker.sh script that builds the images for Docker and pushes them to the repos.Tag is provided as argument

--- Docker/ecr.tf
+++ Docker/ecr.tf
@@ -3,10 +3,58 @@ provider "aws" {
     profile = var.PROFILE
 }
 
-resource "aws_ecr_repository" "docker_repo" {
-    name = "docker_repo"
+resource "aws_ecr_repository" "cadvisor_repo" {
+    name = "cadvisor_repo"
 }
 
-output "docker_repo_url" {
-    value = aws_ecr_repository.docker_repo.repository_url
+resource "aws_ecr_repository" "grafana_repo" {
+    name = "grafana_repo"
+}
+
+resource "aws_ecr_repository" "jenkins_master_repo" {
+    name = "jenkins_master_repo"
+}
+
+resource "aws_ecr_repository" "jenkins_slave_repo" {
+    name = "jenkins_slave_repo"
+}
+
+resource "aws_ecr_repository" "node_exporter_repo" {
+    name = "node_exporter_repo"
+}
+
+resource "aws_ecr_repository" "prometheus_repo" {
+    name = "prometheus_repo"
+}
+
+resource "aws_ecr_repository" "jenkins_nginx_repo" {
+    name = "jenkins_nginx_repo"
+}
+
+output "cadvisor_repo_url" {
+    value = aws_ecr_repository.cadvisor_repo.repository_url 
+}
+
+output "grafana_repo_url" {
+    value = aws_ecr_repository.grafana_repo.repository_url 
+}
+
+output "jenkins_master_repo_url" {
+    value = aws_ecr_repository.jenkins_master_repo.repository_url 
+}
+
+output "jenkins_slave_repo_url" {
+    value = aws_ecr_repository.jenkins_slave_repo.repository_url 
+}
+
+output "node_exporter_repo_url" {
+    value = aws_ecr_repository.node_exporter_repo.repository_url 
+}
+
+output "jenkins_nginx_repo_url" {
+    value = aws_ecr_repository.jenkins_nginx_repo.repository_url 
+}
+
+output "prometheus_repo_url" {
+    value = aws_ecr_repository.prometheus_repo.repository_url 
 }
---
+++ Docker/upload_docker.sh
@@ -0,0 +1,32 @@
+if [$1 -eq ""] 
+then
+echo "Please enter a tag when uploading"
+sleep 5
+exit 1
+fi
+
+cadvisor=$(terraform output cadvisor_repo_url)
+grafana_repo_url=$(terraform output grafana_repo_url)
+jenkins_master_repo_url=$(terraform output jenkins_master_repo_url)
+jenkins_slave_repo_url=$(terraform output jenkins_slave_repo_url)
+node_exporter_repo_url=$(terraform output node_exporter_repo_url)
+prometheus_repo_url=$(terraform output prometheus_repo_url)
+jenkins_nginx_repo_url=$(terraform output jenkins_nginx_repo_url)
+
+
+docker build -t $cadvisor:$1 ./Rito_project/cadvisor
+docker build -t $grafana_repo_url:$1 ./Rito_project/grafana
+docker build -t $jenkins_master_repo_url:$1 ./Rito_project/jenkins_master
+docker build -t $jenkins_slave_repo_url:$1 ./Rito_project/jenkins_slave
+docker build -t $node_exporter_repo_url:$1 ./Rito_project/node_exporter
+docker build -t $prometheus_repo_url:$1 ./Rito_project/prometheus
+docker build -t $jenkins_nginx_repo_url:$1 ./Rito_project/jenkins_nginx
+
+docker push $cadvisor
+docker push $grafana_repo_url
+docker push $jenkins_master_repo_url
+docker push $jenkins_slave_repo_url
+docker push $node_exporter_repo_url
+docker push $prometheus_repo_url
+docker push $jenkins_nginx_repo_url
+
