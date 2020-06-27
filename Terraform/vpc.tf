f077aec2294349394e4529c175e84fc59fd8f192
= <=>
2020-06-27 10:45:53 +0300

    Remove useless subnets

--- Terraform/vpc.tf
+++ Terraform/vpc.tf
@@ -21,28 +21,6 @@ resource "aws_subnet" "main-public-1" {
   }
 }
 
-resource "aws_subnet" "main-public-2" {
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.2.0/24"
-  map_public_ip_on_launch = "true"
-  availability_zone       = "eu-west-1b"
-
-  tags = {
-    Name = "main-public-2"
-  }
-}
-
-resource "aws_subnet" "main-public-3" {
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.3.0/24"
-  map_public_ip_on_launch = "true"
-  availability_zone       = "eu-west-1c"
-
-  tags = {
-    Name = "main-public-3"
-  }
-}
-
 ## Create private subnets ##########################################
 
 resource "aws_subnet" "main-private-1" {
@@ -56,27 +34,6 @@ resource "aws_subnet" "main-private-1" {
   }
 }
 
-resource "aws_subnet" "main-private-2" {
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.5.0/24"
-  map_public_ip_on_launch = "false"
-  availability_zone       = "eu-west-1b"
-
-  tags = {
-    Name = "main-private-2"
-  }
-}
-
-resource "aws_subnet" "main-private-3" {
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.6.0/24"
-  map_public_ip_on_launch = "false"
-  availability_zone       = "eu-west-1c"
-
-  tags = {
-    Name = "main-private-3"
-  }
-}
 
 ## Create Internet Gateway ######################################
 
@@ -107,13 +64,4 @@ resource "aws_route_table_association" "main-public-1-a" {
   route_table_id = aws_route_table.main_public.id
 }
 
-resource "aws_route_table_association" "main-public-1-b" {
-  subnet_id      = aws_subnet.main-public-2.id
-  route_table_id = aws_route_table.main_public.id
-}
-
-resource "aws_route_table_association" "main-public-1-c" {
-  subnet_id      = aws_subnet.main-public-3.id
-  route_table_id = aws_route_table.main_public.id
-}
 
