23a82368050c7a98591b7ca2e3d5ffd5545a59a7
= <=>
2020-06-27 12:55:31 +0300

    fix ebs mounting...

--- Terraform/files/update.sh
+++ Terraform/files/update.sh
@@ -2,5 +2,5 @@
 sudo mkfs.ext4 /dev/xvdh
 sudo mkdir /data
 sudo mount /dev/xvdh /data
-sudo chmod +w /etc/fstab
-echo "sudo /dev/xvdh /data ext4 defaults 0 0" >> /etc/fstab
+sudo chmod 007 /etc/fstab
+sudo echo "/dev/xvdh /data ext4 defaults 0 0" >> /etc/fstab
