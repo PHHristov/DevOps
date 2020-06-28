b9ad61c177f804c2aba8ce8c027022cafbe3fce5
= <=>
2020-06-28 17:41:57 +0300

    add security groups to allow grafana, prometheus, node exporter and cadvisor

--- Terraform/securitygroup.tf
+++ Terraform/securitygroup.tf
@@ -39,3 +39,109 @@ resource "aws_security_group" "allow_ssh" {
     Name = "allow_ssh"
   }
 }
+
+resource "aws_security_group" "allow_jenkins" {
+  name   = "allow_jenkins"
+  vpc_id = aws_vpc.main.id
+  ingress {
+    from_port   = "8080"
+    to_port     = "8080"
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = "0"
+    to_port     = "0"
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_Jenkins"
+  }
+}
+
+
+resource "aws_security_group" "allow_node_exporter" {
+  name   = "allow_node_exporter"
+  vpc_id = aws_vpc.main.id
+  ingress {
+    from_port   = "9100"
+    to_port     = "9100"
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = "0"
+    to_port     = "0"
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_node_exporter"
+  }
+}
+
+resource "aws_security_group" "allow_cadvisor" {
+  name   = "allow_cadvisor"
+  vpc_id = aws_vpc.main.id
+  ingress {
+    from_port   = "8069"
+    to_port     = "8069"
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = "0"
+    to_port     = "0"
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_cadvisor"
+  }
+}
+
+resource "aws_security_group" "allow_prometheus" {
+  name   = "allow_prometheus"
+  vpc_id = aws_vpc.main.id
+  ingress {
+    from_port   = "9090"
+    to_port     = "9090"
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = "0"
+    to_port     = "0"
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_prometheus"
+  }
+}
+
+resource "aws_security_group" "allow_grafana" {
+  name   = "allow_grafana"
+  vpc_id = aws_vpc.main.id
+  ingress {
+    from_port   = "3000"
+    to_port     = "3000"
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = "0"
+    to_port     = "0"
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_grafana"
+  }
+}
