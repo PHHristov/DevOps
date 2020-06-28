3db0ba9288805a25c93d4fc0a76ffb9c998aa4dd
= <=>
2020-06-28 22:38:12 +0300

    Install Ansible

--- Terraform/files/update.sh
+++ Terraform/files/update.sh
@@ -1,6 +1,11 @@
-#!/bin/bash
-sudo mkfs.ext4 /dev/xvdh
-sudo mkdir /data
-sudo mount /dev/xvdh /data
-sudo chmod 007 /etc/fstab
-sudo echo "/dev/xvdh /data ext4 defaults 0 0" >> /etc/fstab
+# Install ansible
+sudo apt-get update
+sudo apt-get install software-properties-common
+sudo apt-add-repository --yes --update ppa:ansible/ansible
+sudo apt-get install ansible --yes
+
+
+#Install Docker Compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo apt install docker-compose --yes
