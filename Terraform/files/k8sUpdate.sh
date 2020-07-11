6467b2eca1a1fbafd9ec90bc8d2e9416a6209c27
= <=>
2020-07-11 14:03:19 +0300

    install minikube

--- Terraform/files/k8sUpdate.sh
+++ Terraform/files/k8sUpdate.sh
@@ -1,5 +1,10 @@
-# Install ansible
+## Install ansible
 sudo apt-get update
 sudo apt-get install software-properties-common
 sudo apt-add-repository --yes --update ppa:ansible/ansible
 sudo apt-get install ansible --yes
+## Install minikube
+sudo curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 \
+ && chmod +x minikube
+sudo mkdir -p /usr/local/bin/
+sudo install minikube /usr/local/bin/
