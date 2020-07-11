3473e595332d3072e7a020849cf5a762fd22e1e5
= <=>
2020-07-11 15:05:24 +0300

    k8s stuff2 ...

--- Terraform/files/k8sUpdate.sh
+++ Terraform/files/k8sUpdate.sh
@@ -6,5 +6,7 @@ sudo apt-get install ansible --yes
 ## Install minikube
 sudo curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 \
  && chmod +x minikube
+curl -o kubectl https://amazon-eks.s3.us-west-2.amazonaws.com/1.16.8/2020-04-16/bin/linux/amd64/kubectl
 sudo mkdir -p /usr/local/bin/
 sudo install minikube /usr/local/bin/
+systemctl enable kubelet.service
