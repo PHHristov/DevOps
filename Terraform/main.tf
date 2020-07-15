f50e460f22bfe8cd50d49fc03afbc80cb3d648b5
PHRISTOV@visteon.com <PHRISTOV@visteon.com>
2020-07-15 14:19:07 +0300

    Uncomment Jenkins master

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -12,66 +12,66 @@ resource "aws_key_pair" "mykey" {
 ## Jenkins MASTER #########################################################
 ###########################################################################
 
-## resource "aws_instance" "web" {
-##   ami                         = var.AMIS[var.AWS_REGION]
-##   instance_type               = "t2.micro"
-##   key_name                    = aws_key_pair.mykey.key_name
-##   associate_public_ip_address = true
-##   ## VPC Subnet 
-##   subnet_id = aws_subnet.main-public-1.id
-##   ## Security Groups
-##   vpc_security_group_ids = [aws_security_group.allow_ssh.id,
-##                             aws_security_group.allow_jenkins.id,
-##                             aws_security_group.allow_grafana.id,
-##                             aws_security_group.allow_prometheus.id,
-##                             aws_security_group.allow_cadvisor.id,
-##                             aws_security_group.allow_node_exporter.id,
-##                             ]
-## 
-##   user_data = "sudo apt-get update -y \n apt-get install git-core"
-## 
-##   tags = {
-##     Name = "olala_danette"
-##   }
-## 
-##   provisioner "file" {
-##     source      = "./files/update.sh"
-##     destination = "/var/tmp/update.sh"
-##   }
-## 
-##   provisioner "file" {
-##     source      = "./files/ebsfs.sh"
-##     destination = "/var/tmp/ebsfs.sh"
-##   }
-## 
-##   provisioner "remote-exec" {
-##     inline = [
-##       "cd /usr/src/",
-##       "sudo git clone https://github.com/PHHristov/DevOps.git",
-##       "sleep 5",
-##       "sudo chmod -R +x /usr/src/*",
-##       "cd /usr/src/DevOps/Terraform/files",
-##       "sleep 5",
-##       "sudo ./update.sh",
-##       #"sleep 5",
-##       #"sudo ./ebsfs.sh",
-##       "sleep 5",
-##       "cd /usr/src/DevOps/Ansible",
-##       "ansible-playbook master.yml",
-##       "cd /usr/src/DevOps/Docker/Rito_Project/",
-##       "sudo docker-compose up -d"
-##     ]
-##   }
-## 
-## 
-##   connection {
-##     host        = coalesce(self.public_ip, self.private_ip)
-##     user        = var.instance_username
-##     private_key = file(var.PATH_TO_PRIVATE_KEY)
-## 
-##   }
-## 
-## }
+resource "aws_instance" "web" {
+  ami                         = var.AMIS[var.AWS_REGION]
+  instance_type               = "t2.micro"
+  key_name                    = aws_key_pair.mykey.key_name
+  associate_public_ip_address = true
+  ## VPC Subnet 
+  subnet_id = aws_subnet.main-public-1.id
+  ## Security Groups
+  vpc_security_group_ids = [
+      aws_security_group.allow_ssh.id,
+      aws_security_group.allow_jenkins.id,
+      aws_security_group.allow_grafana.id,
+      aws_security_group.allow_prometheus.id,
+      aws_security_group.allow_cadvisor.id,
+      aws_security_group.allow_node_exporter.id,]
+
+  user_data = "sudo apt-get update -y \n apt-get install git-core"
+
+  tags = {
+    Name = "olala_danette"
+  }
+
+  provisioner "file" {
+    source      = "./files/update.sh"
+    destination = "/var/tmp/update.sh"
+  }
+
+  provisioner "file" {
+    source      = "./files/ebsfs.sh"
+    destination = "/var/tmp/ebsfs.sh"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "cd /usr/src/",
+      "sudo git clone https://github.com/PHHristov/DevOps.git",
+      "sleep 5",
+      "sudo chmod -R +x /usr/src/*",
+      "cd /usr/src/DevOps/Terraform/files",
+      "sleep 5",
+      "sudo ./update.sh",
+      #"sleep 5",
+      #"sudo ./ebsfs.sh",
+      "sleep 5",
+      "cd /usr/src/DevOps/Ansible",
+      "ansible-playbook master.yml",
+      "cd /usr/src/DevOps/Docker/Rito_Project/",
+      "sudo docker-compose up -d"
+    ]
+  }
+
+
+  connection {
+    host        = coalesce(self.public_ip, self.private_ip)
+    user        = var.instance_username
+    private_key = file(var.PATH_TO_PRIVATE_KEY)
+
+  }
+
+}
 
 ###########################################################################
 ## Kubernetes MASTER ######################################################
@@ -85,13 +85,13 @@ resource "aws_instance" "Kube" {
   ## VPC Subnet 
   subnet_id = aws_subnet.main-public-1.id
   ## Security Groups
-  vpc_security_group_ids = [aws_security_group.allow_ssh.id,
-                            aws_security_group.allow_jenkins.id,
-                            aws_security_group.allow_grafana.id,
-                            aws_security_group.allow_prometheus.id,
-                            aws_security_group.allow_cadvisor.id,
-                            aws_security_group.allow_node_exporter.id,
-                            ]
+  vpc_security_group_ids = [
+      aws_security_group.allow_ssh.id,
+      aws_security_group.allow_jenkins.id,
+      aws_security_group.allow_grafana.id,
+      aws_security_group.allow_prometheus.id,
+      aws_security_group.allow_cadvisor.id,
+      aws_security_group.allow_node_exporter.id,]
 
   user_data = "sudo apt-get update -y \n apt-get install git-core \n export PATH=/home/ubuntu/.linuxbrew/bin:$PATH"
 
@@ -156,10 +156,13 @@ resource "aws_instance" "Kube" {
 ###########################################################################
 
 
-output "ip" {
+output "Kube_ip" {
   value = aws_instance.Kube.public_ip
 }
 
+output "Jenkins_ip" {
+  value = aws_instance.web.public_ip
+}
 
 
 
