d7e3176869870fa474cc61f07fefb462fba17091
= <=>
2020-07-25 15:05:04 +0300

    Fix kubectl install

--- Terraform/files/k8sUpdate.sh
+++ Terraform/files/k8sUpdate.sh
@@ -3,6 +3,11 @@ sudo apt-get update
 sudo apt-get install software-properties-common
 sudo apt-add-repository --yes --update ppa:ansible/ansible
 sudo apt-get install ansible --yes
+
+## install kubectl
+sudo curl -o kubectl https://amazon-eks.s3.us-west-2.amazonaws.com/1.16.8/2020-04-16/bin/linux/amd64/kubectl
+sudo chown -R ubuntu /usr/local/bin/minikube
+sudo usermod -aG docker ubuntu && newgrp docker
 ## Install minikube
 sudo curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 \
  && chmod +x minikube
@@ -11,6 +16,3 @@ sudo mkdir -p /usr/local/bin/
 sudo install minikube /usr/local/bin/
 systemctl enable kubelet.service
 
-curl -o kubectl https://amazon-eks.s3.us-west-2.amazonaws.com/1.16.8/2020-04-16/bin/linux/amd64/kubectl
-sudo chown -R ubuntu /usr/local/bin/minikube
-sudo usermod -aG docker ubuntu && newgrp docker
