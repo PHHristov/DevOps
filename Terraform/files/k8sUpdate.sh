6035d62b3736f39f1d430ea82fff111172f53034
= <=>
2020-07-26 10:12:47 +0300

    try to workaround minikube failure

--- Terraform/files/k8sUpdate.sh
+++ Terraform/files/k8sUpdate.sh
@@ -19,4 +19,11 @@ sudo curl -o kubectl https://amazon-eks.s3.us-west-2.amazonaws.com/1.16.8/2020-0
 chmod 777 ./kubectl
 sudo mv ./kubectl /usr/local/bin/kubectl
 
+## AWS ECR login
+export AWS_ACCESS_KEY_ID="AKIA36RLNVBTJVDLNLOG"
+export AWS_SECRET_ACCESS_KEY="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"
+export AWS_DEFAULT_REGION="eu-west-1"
+aws ecr get-login-password --region eu-west-1  | docker login  --username AWS  --password-stdin 821503567974.dkr.ecr.eu-west-1.amazonaws.com/backend
+aws ecr get-login-password --region eu-west-1  | docker login  --username AWS  --password-stdin 821503567974.dkr.ecr.eu-west-1.amazonaws.com/frontend
+
 
